e0ce95bfa725087f45c8ec7910834418c8bbbb59
Asha Raghav <z5363204@ad.unsw.edu.au>
2024-05-12 22:32:28 +1000

    initial commit

---
+++ asha-web/src/App.js
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+// import React from 'react';
+
+import './App.css';
+import Blog from './blog/Blog';
+function App() {
+  return (
+    <div className="App">
+    <Blog /> {/* Render the Blog component */}
+  </div>
+);
+}
+
+export default App;
